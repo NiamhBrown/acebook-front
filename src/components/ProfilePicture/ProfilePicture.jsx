411f175379072f656063e385e3d7a1a9c42bc6e7
niamh <niamh321@me.com>
2024-07-19 10:15:08 +0100

    ProfilePicture component now works successfully, for both signed in and other users.

--- src/components/ProfilePicture/ProfilePicture.jsx
+++ src/components/ProfilePicture/ProfilePicture.jsx
@@ -1,11 +1,30 @@
 import "../../../css/main.css";
+import { useState, useEffect } from "react";
+import { getSignedInUser, getUser } from "../../services/users";
 import defaultProfilePicture from "../../assets/default_picture.png";
 
-export const ProfilePicture = ({signedInUser }) => {
-  const bucketBaseUrl =
-    "https://my-acebook-bucket.s3.eu-north-1.amazonaws.com/";
-  const profilePictureKey = signedInUser.profilePicture;
-  const profileImageUrl = `${bucketBaseUrl}${profilePictureKey}`;
+export const ProfilePicture = ({ userId }) => {
+  const [profilePictureUrl, setProfilePictureUrl] = useState(
+    defaultProfilePicture
+  );
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (token) {
+      getUser(userId, token)
+        .then((data) => {
+          console.log("NEW SEVICE-data", data.user.profilePicture);
+            const bucketBaseUrl =
+              "https://my-acebook-bucket.s3.eu-north-1.amazonaws.com/";
+          const profilePictureKey = data.user.profilePicture;
+          setProfilePictureUrl(`${bucketBaseUrl}${profilePictureKey}`);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, []);
+;
 
   const handleError = (event) => {
     event.target.src = defaultProfilePicture;
@@ -13,7 +32,7 @@ export const ProfilePicture = ({signedInUser }) => {
 
   return (
     <img
-      src={profileImageUrl}
+      src={profilePictureUrl}
       alt="User's profile pic"
       onError={handleError}
       className="circular-image"
